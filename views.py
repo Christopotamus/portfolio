e9cf4aa08df9cca9a2047ac74ff2fc6f26640ea1
Christopher Budd <chris.cyborg@gmail.com>
2012-02-29 17:27:10 -0800

    Removed compiled python files and created some html files.

--- views.py
+++ views.py
@@ -5,8 +5,8 @@ from django.shortcuts import render_to_response
 def home(request):
    return render_to_response('home.html',locals(),context_instance=RequestContext(request))
 def resume(request):
-   return render_to_response('home.html',locals(),context_instance=RequestContext(request))
+   return render_to_response('resume.html',locals(),context_instance=RequestContext(request))
 def showcase(request):
-   return render_to_response('home.html',locals(),context_instance=RequestContext(request))
+   return render_to_response('showcase.html',locals(),context_instance=RequestContext(request))
 def contact(request):
-   return render_to_response('home.html',locals(),context_instance=RequestContext(request))
+   return render_to_response('contact.html',locals(),context_instance=RequestContext(request))
