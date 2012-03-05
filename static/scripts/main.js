b8dc7649f01db7213d9d8dd561e03dfd7e9c505c
Christopher Budd <chris.cyborg@gmail.com>
2012-03-04 19:47:29 -0800

    Moved Resume CSS into it's own file. Made clickable sections on resume page with animated container heights.

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,5 +1,35 @@
+var currentResumeSection = 'objective';
+var topID = '';
+
 $(function(){
+    $(".resume-section-body").animate({height: "toggle"}, 0);
+
+    $("#objective-body").animate({height: "toggle"}, 'slow');    
+    $("#objective-expand").html("-");
+
     $(".bottom-nav-link, .main-nav-link").click(function(){
         window.location.href = this.id;
     });
+    
+    $(".resume-section-expand").click(function(){
+        var expandTab = $('#'+currentResumeSection+'-expand')[0];
+        if(expandTab.innerHTML == '+')
+            expandTab.innerHTML = '-';
+        else
+            expandTab.innerHTML = '+';
+
+        topID = this.id.split('-')[0];
+        $("#"+currentResumeSection+"-body").animate({height:"toggle"}, 'slow', function(){
+            currentResumeSection = topID;
+            var expandTab = $('#'+topID+'-expand')[0];
+            if(expandTab.innerHTML == '+')
+                expandTab.innerHTML = '-';
+            else
+                expandTab.innerHTML = '+';
+
+            var resumeBody = $('#'+topID+"-body")[0];
+            $(resumeBody).animate({height: "toggle"}, 'slow');
+        });
+    });
 });
+
