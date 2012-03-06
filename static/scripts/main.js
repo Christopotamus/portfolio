a34b48ce3cfb6b04050a8679d5012fb0d85c6311
Christopher Budd <chris.cyborg@gmail.com>
2012-03-05 19:54:30 -0800

    Added some fancy arrows to the nav links and styled appropriately.

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,13 +1,17 @@
 var currentResumeSection = (window.location.hash.length > 0) ? window.location.hash.split("#")[1] : "objective";
 var topID = '';
+var currentPage = window.location.pathname.split("/")[1]+'\/';
 
 $(function(){
+    //console.log(currentPage);
+    $(".main-nav-link#"+currentPage).addClass("active-link").append("<div class='arrow-down'></div>");
     $(".resume-section-body").animate({height: "toggle"}, 0);
 
     expandSection(currentResumeSection);
     $(".bottom-nav-link, .main-nav-link").click(function(){
+        //console.log(this.id.toString().substring(0,idLen -1));
+        //$(".main-nav-link, #"+
         window.location.hash = '';
-        console.log(window.location);
         window.location.pathname = this.id;
     });
     
