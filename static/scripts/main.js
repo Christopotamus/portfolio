bf41db7ae91cb1561a1947828d97a94068d77d5d
Christopher Budd <chris.cyborg@gmail.com>
2012-03-05 23:08:24 -0800

    Moved all resume javascript into it's own file included in resume template. Moved resume headers to a nav bar on the side of the content-block    (note for future: change resume-section-head to resume-section-nav or something)

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,47 +1,12 @@
-var currentResumeSection = (window.location.hash.length > 0) ? window.location.hash.split("#")[1] : "objective";
-var topID = '';
-var currentPage = window.location.pathname.split("/")[1]+'\/';
-
 $(function(){
-    //console.log(currentPage);
-    $(".main-nav-link#"+currentPage).addClass("active-link").append("<div class='arrow-down'></div>");
-    $(".resume-section-body").animate({height: "toggle"}, 0);
+      
+var currentPage = window.location.pathname.split("/")[1]+'\/'; 
+   $(".main-nav-link#"+currentPage).addClass("active-link drop-shadow").append("<div class='arrow-down'></div>");
 
-    expandSection(currentResumeSection);
-    $(".bottom-nav-link, .main-nav-link").click(function(){
-        //console.log(this.id.toString().substring(0,idLen -1));
-        //$(".main-nav-link, #"+
-        window.location.hash = '';
-        window.location.pathname = this.id;
-    });
-    
-    $(".resume-section-expand").click(function(){
-        var expandTab = $('#'+currentResumeSection+'-expand')[0];
-        if(expandTab.innerHTML == '+')
-            expandTab.innerHTML = '-';
-        else
-            expandTab.innerHTML = '+';
-
-        topID = this.id.split('-')[0];
-        $("#"+currentResumeSection+"-body").animate({height:"toggle"}, 'slow', function(){
-            currentResumeSection = topID;
-            var expandTab = $('#'+topID+'-expand')[0];
-            window.location.hash = topID;
-            if(expandTab.innerHTML == '+')
-                expandTab.innerHTML = '-';
-            else
-                expandTab.innerHTML = '+';
-
-            var resumeBody = $('#'+topID+"-body")[0];
-            $(resumeBody).animate({height: "toggle"}, 'slow');
-        });
-    });
+   $(".bottom-nav-link, .main-nav-link").click(function(){
+      //console.log(this.id.toString().substring(0,idLen -1));
+      //$(".main-nav-link, #"+
+      window.location.hash = '';
+      window.location.pathname = this.id;
+   });
 });
-
-function expandSection(sectionName){
-    $("#"+sectionName+"-body").animate({height: "toggle"}, 'slow');    
-    $("#"+sectionName+"-expand").html("-");
-}
-function closeSection(sectionName){
-
-}
