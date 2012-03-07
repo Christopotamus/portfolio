64b41adf96e4f20bbb4bbe8b487416d6d4ce5b90
Christopher Budd <chris.cyborg@gmail.com>
2012-03-06 19:50:30 -0800

    Added nivoSlider showcase and a .gif loader.

--- views.py
+++ views.py
@@ -4,13 +4,18 @@ from django.shortcuts import render_to_response, redirect
 
 def redirHome(request):
    return redirect('/home/');
+
 def home(request):
    return render_to_response('home.html',locals(),context_instance=RequestContext(request))
+
 def resume(request):
    if 'section' in request.GET:
       currentSection = request.GET['section']   
    return render_to_response('resume.html',locals(),context_instance=RequestContext(request))
+
 def showcase(request):
+   sitePictures = ["logo.png"]
    return render_to_response('showcase.html',locals(),context_instance=RequestContext(request))
+
 def contact(request):
    return render_to_response('contact.html',locals(),context_instance=RequestContext(request))
