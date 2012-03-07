eabb4c00b28a9bcdb8419385f0f0110efc49177b
Christopher Budd <chris.cyborg@gmail.com>
2012-03-06 21:35:12 -0800

    Added some niceypants scrolling action from the navbar. Started a footer section that will take the place of contacts

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,12 +1,57 @@
+var clickedLink = '';
+
 $(function(){
       
-var currentPage = window.location.pathname.split("/")[1]+'\/'; 
+   var currentPage = window.location.pathname.split("/")[1]+'\/'; 
    $(".main-nav-link#"+currentPage).addClass("active-link drop-shadow").append("<div class='arrow-down'></div>");
 
    $(".bottom-nav-link, .main-nav-link").click(function(){
-      //console.log(this.id.toString().substring(0,idLen -1));
-      //$(".main-nav-link, #"+
-      window.location.hash = '';
-      window.location.pathname = this.id;
+      clickedLink = this.id;
+      console.log($('html').scrollTop());
+      if($(document).scrollTop() > 150)
+         scrollAndNav(clickedLink);   
+      else
+         noScrollAndNav(clickedLink);
+   });
+
+   $(".main-contact-nav, .bottom-contact-nav").click(function(){
+      anchorPos = $("#footer").offset().top;
+      currentPage = "contact";
+      $(".main-nav-link").removeClass("active-link drop-shadow");
+      $(".arrow-down").remove();
+      $(".main-contact-nav").addClass("active-link drop-shadow").append("<div class='arrow-down'></div>");
+      $('html,body').animate({scrollTop:anchorPos}, 1500, 'easeInOutExpo');
    });
 });
+
+function needToChangePage(clickedPage){
+   splitPath = window.location.pathname.split("/")[1];
+   if(clickedPage != splitPath)
+      return true;
+   else
+      return false;
+}
+function scrollAndNav(clickedLink){
+      $('html,body').animate({scrollTop:0}, 1500, 'easeInOutExpo',function(){
+         if(needToChangePage(clickedLink) == true){
+            window.location.hash = '';
+            window.location.pathname = clickedLink;
+         }else{
+
+            $(".main-contact-nav").removeClass("active-link drop-shadow");
+            $(".arrow-down").remove();
+            $(".main-nav-link#"+clickedLink).addClass("active-link drop-shadow").append("<div class='arrow-down'></div>");
+         }
+      });
+}
+function noScrollAndNav(clickedLink){
+   if(needToChangePage(clickedLink) == true){
+      window.location.hash = '';
+      window.location.pathname = clickedLink;
+   }else{
+
+      $(".main-contact-nav").removeClass("active-link drop-shadow");
+      $(".arrow-down").remove();
+      $(".main-nav-link#"+clickedLink).addClass("active-link drop-shadow").append("<div class='arrow-down'></div>");
+   }
+}
