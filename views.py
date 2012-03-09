cf744ad0a150b7c0c6b9f419655221ddb71a9464
Christopher Budd <chris.cyborg@gmail.com>
2012-03-09 10:33:45 -0800

    Removed "homepage" and made the showcase the default main page.

--- views.py
+++ views.py
@@ -4,10 +4,10 @@ from django.shortcuts import render_to_response, redirect
 from django.core.mail import send_mail
 
 def redirHome(request):
-   return redirect('/home/');
+   return redirect('/showcase/');
 
 def home(request):
-   return render_to_response('home.html',locals(),context_instance=RequestContext(request))
+   return render_to_response('showcase.html',locals(),context_instance=RequestContext(request))
 
 def resume(request):
    if 'section' in request.GET:
